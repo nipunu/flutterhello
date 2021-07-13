659a40c2a9c51183970b75d19316eb3426ea87cb
Nipunu Wijesingha <nipunu.wijesingha@gmail.com>
2021-07-13 13:55:39 +0530

    initial commit to master

---
+++ lib/helloFluterApp.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+import 'package:hello_flutter/screens/home.dart';
+
+class HelloFlutterApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+   return MaterialApp(
+     debugShowCheckedModeBanner: false,
+      title: "Hello Flutter App",
+      home: Scaffold(
+        appBar: AppBar(title: Text("My Flutter App"),),
+        body: new home(),
+      ),
+    );
+  }
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:hello_flutter/helloFluterApp.dart';
+
+/*
+void main(){
+  runApp(
+       new HelloFlutterApp(),
+    );
+}
+*/
+
+void main() => runApp(new HelloFlutterApp());
